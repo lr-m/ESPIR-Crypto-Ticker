c51373879d063ab27127793c57d4a0389eb9a659
Luke Mills <47477832+luke-r-mills@users.noreply.github.com>
2022-03-31 23:42:59 +0100

    Update ticker.ino

--- ticker/ticker.ino
+++ ticker/ticker.ino
@@ -81,7 +81,7 @@ int portfolio_time_slot_moved =
 // For HTTP connection
 WiFiClientSecure client;
 HTTPClient http;
-const char *fingerprint = "33c57b69e63b765c393df1193b1768b81b0a1fd9";
+const char *fingerprint = "*Your fingerprint here";
 char *url_start = "https://api.coingecko.com/api/v3/simple/price?ids=";
 char *url_end = "&vs_currencies=gbp&include_24hr_change=true";
 
